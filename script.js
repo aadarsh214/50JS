17fd12fefaa9a407b61bd57b22b7bd2f77b69360
aadarsh214 <aadarshgupta7828@gmail.com>
2025-02-10 14:53:12 +0530

    replace for-in loop with forEach to log array values

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-let obj = {
-    name: 'John',
-    age: 30,
-    isMarried: false
-}
-
-for (let key in obj){{
-    console.log(key)
-    console.log(obj[key])
-}}
+let arr = ["a", "b", "c"];
+arr.forEach(function(value){
+    console.log(value);
+})
