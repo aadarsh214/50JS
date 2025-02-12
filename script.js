eeb3e8b07bf85803174fa515d46ab18d81abc67c
aadarsh214 <aadarshgupta7828@gmail.com>
2025-02-12 17:24:56 +0530

    add function to determine the greater of two numbers

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function checker(str,char){
-    return str.endsWith(char);
+function greaterThan(a,b){
+    if(a>b){
+        return a
+    }
+    else{
+        return b
+    }
 }
 
-console.log(checker("hello","o")); // true
+console.log(greaterThan(99,5)) // Output: 5
